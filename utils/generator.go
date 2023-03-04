33b62d9ee11e2b5e3df23cc1a684dcf8bf1ed72e
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2023-03-04 18:07:39 +0100

    Add generator

---
+++ utils/generator.go
@@ -0,0 +1,12 @@
+package utils
+
+func Generator[T any](slice []T) <-chan T {
+	ch := make(chan T)
+	go func() {
+		defer close(ch)
+		for _, element := range slice {
+			ch <- element
+		}
+	}()
+	return ch
+}
