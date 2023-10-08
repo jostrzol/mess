2af8a614d354871e294a264e966025176dac3dff
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2023-10-08 16:04:18 +0200

    factor out tile

--- src/app/components/game/board.tsx
+++ src/app/components/game/board.tsx
@@ -1,10 +1,8 @@
 import { Board as BoardModel } from "@/app/model/board";
-import { Color } from "@/app/model/color";
 import { Piece as PieceModel } from "@/app/model/piece";
 import { Square } from "@/app/model/square";
-import clsx from "clsx";
 import { useState } from "react";
-import { Piece } from "./piece";
+import { Tile } from "./tile";
 
 export type BoardProps = {
   pieces: PieceModel[];
@@ -43,69 +41,6 @@ export const Board = ({ board, pieces }: BoardProps) => {
           );
         }),
       )}
-    </div>
-  );
-};
-
-type TileProps = {
-  color: Color;
-  piece?: PieceModel;
-  onPieceHovered?: (piece: PieceModel) => any;
-  onPieceUnhovered?: (piece: PieceModel) => any;
-  isMoveProjected?: boolean;
-};
-
-const Tile = ({
-  color,
-  piece,
-  onPieceHovered,
-  onPieceUnhovered,
-  isMoveProjected = false,
-}: TileProps) => {
-  return (
-    <div
-      className={clsx(
-        "min-h-[3rem]",
-        "min-w-[3rem]",
-        color == "white" ? "bg-player-white" : "bg-player-black",
-        "rounded-2xl",
-        "relative",
-        piece && "hover:cursor-pointer",
-      )}
-      style={{
-        overflowClipMargin: "content-box",
-        overflow: "clip",
-      }}
-    >
-      <div
-        className={clsx(
-          "absolute",
-          "z-10",
-          "top-1/2",
-          "left-1/2",
-          "-translate-x-1/2",
-          "-translate-y-1/2",
-          "w-4",
-          "h-4",
-          "rounded-full",
-          piece ? "bg-danger" : "bg-primary/80",
-          "transition-opacity",
-          isMoveProjected || "opacity-0",
-          "pointer-events-none",
-        )}
-      />
-      <div
-        className={clsx("hover:scale-110", "transition-transform")}
-        onPointerEnter={() => piece && onPieceHovered?.(piece)}
-        onPointerLeave={() => piece && onPieceUnhovered?.(piece)}
-      >
-        {piece && <Piece piece={piece} />}
-      </div>
-      {/* Needed to make the parent div expand.
-      Coulnd't get it to work without the image */}
-      <svg className="invisible">
-        <rect width="1" height="1" />
-      </svg>
     </div>
   );
 };
