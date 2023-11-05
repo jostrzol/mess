1127a4969d14207d2f41db4ee936f03a4173af15
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2023-11-05 12:47:30 +0100

    room tests

--- be/pkg/server/adapter/handler/handlertest/handlertest.go
+++ be/pkg/server/adapter/handler/handlertest/handlertest.go
@@ -8,6 +8,7 @@ import (
 	"net/http/cookiejar"
 	"net/http/httptest"
 	"net/url"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jostrzol/mess/configs/serverconfig"
@@ -31,52 +32,86 @@ func SetupRouter() *gin.Engine {
 	return ioc.MustResolve[*gin.Engine]()
 }
 
-type HandlerSuite struct {
+type HandlerSuite[T Client] struct {
 	suite.Suite
+	defaultClient *T
+	g             *gin.Engine
+}
+
+func (s *HandlerSuite[T]) SetupTest() {
+	s.g = SetupRouter()
+	s.defaultClient = s.NewClient()
+}
+
+func (s *HandlerSuite[T]) Client() *T {
+	return s.defaultClient
+}
+
+func (s *HandlerSuite[T]) NewClient() *T {
+	var client T
+	v := reflect.Indirect(reflect.ValueOf(&client))
+	v.FieldByName("BaseClient").Set(reflect.ValueOf(new(BaseClient)))
+	client.initClient(newHTTPClient(&s.Suite, s.g))
+	return &client
+}
+
+type Client interface {
+	initClient(*httpClient)
+}
+
+type BaseClient struct {
+	*httpClient
+}
+
+func (c *BaseClient) initClient(httpClient *httpClient) {
+	c.httpClient = httpClient
+}
+
+type httpClient struct {
+	*suite.Suite
 	g   *gin.Engine
 	jar *cookiejar.Jar
 }
 
-func (s *HandlerSuite) SetupTest() {
-	var err error
-	s.g = SetupRouter()
-	s.jar, err = cookiejar.New(nil)
+func newHTTPClient(suite *suite.Suite, g *gin.Engine) *httpClient {
+	jar, err := cookiejar.New(nil)
 	if err != nil {
 		panic(err)
 	}
+	return &httpClient{Suite: suite, g: g, jar: jar}
 }
 
-func (s *HandlerSuite) ServeHTTPOkAs(method string, url string, body JSON, result interface{}) {
-	res := s.ServeHTTPOk(method, url, body)
+func (c *httpClient) ServeHTTPOkAs(method string, url string, body JSON, result interface{}) {
+	res := c.ServeHTTPOk(method, url, body)
 	bytes, err := io.ReadAll(res.Result().Body)
-	s.NoError(err)
+	c.NoError(err)
 
 	err = json.Unmarshal(bytes, &result)
-	s.NoError(err)
+	c.NoError(err)
 }
 
-func (s *HandlerSuite) ServeHTTPOk(method string, url string, body JSON) *httptest.ResponseRecorder {
-	res := s.ServeHTTP(method, url, body)
-	s.Equal(res.Result().StatusCode, http.StatusOK)
+func (c *httpClient) ServeHTTPOk(method string, url string, body JSON) *httptest.ResponseRecorder {
+	res := c.ServeHTTP(method, url, body)
+	c.Equal(res.Result().StatusCode, http.StatusOK)
 	return res
 }
 
-func (s *HandlerSuite) ServeHTTP(method string, url string, body JSON) *httptest.ResponseRecorder {
+func (c *httpClient) ServeHTTP(method string, url string, body JSON) *httptest.ResponseRecorder {
 	res := httptest.NewRecorder()
 
 	bodyBytes, err := json.Marshal(body)
-	s.NoError(err)
+	c.NoError(err)
 	req, err := http.NewRequest(method, url, bytes.NewReader(bodyBytes))
-	for _, cookie := range s.jar.Cookies(&root) {
+	for _, cookie := range c.jar.Cookies(&root) {
 		req.AddCookie(cookie)
 	}
-	s.NoError(err)
+	c.NoError(err)
 
-	s.g.ServeHTTP(res, req)
-	s.jar.SetCookies(&root, res.Result().Cookies())
+	c.g.ServeHTTP(res, req)
+	c.jar.SetCookies(&root, res.Result().Cookies())
 	return res
 }
 
 type JSON = map[string]any
 
-var root = url.URL{Path: "/"}
+var root = url.URL{Scheme: "http", Path: "/"}
--- be/pkg/server/adapter/handler/room_test.go
+++ be/pkg/server/adapter/handler/room_test.go
@@ -3,25 +3,59 @@ package handler_test
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/jostrzol/mess/pkg/server/adapter/handler/handlertest"
 	"github.com/jostrzol/mess/pkg/server/adapter/schema"
 	"github.com/stretchr/testify/suite"
 )
 
 type JSON = handlertest.JSON
-type RoomSuite struct{ handlertest.HandlerSuite }
+type RoomSuite struct {
+	handlertest.HandlerSuite[RoomClient]
+}
 
 func (s *RoomSuite) TestCreateRoom() {
-	s.createRoom()
+	s.Client().createRoom()
 }
 
-func (s *RoomSuite) TestJoinRoom() {
-	s.createRoom()
+func (s *RoomSuite) TestJoinRoomSameClient() {
+	room := s.Client().createRoom()
+	room = s.Client().joinRoom(room.ID)
+	s.Equal(1, room.Players)
 }
 
-func (s *RoomSuite) createRoom() (room schema.Room) {
-	s.ServeHTTPOkAs("POST", "/rooms", nil, &room)
+func (s *RoomSuite) TestJoinRoomDifferentClient() {
+	room := s.Client().createRoom()
+
+	c2 := s.NewClient()
+	room = c2.joinRoom(room.ID)
+	s.Equal(2, room.Players)
+}
+
+func (s *RoomSuite) TestGetRoom() {
+	room := s.Client().createRoom()
+	s.Client().getRoom(room.ID)
+}
+
+type RoomClient struct{ *handlertest.BaseClient }
+
+func (c *RoomClient) createRoom() (room schema.Room) {
+	c.ServeHTTPOkAs("POST", "/rooms", nil, &room)
 	return
+}
+
+func (c *RoomClient) joinRoom(roomID uuid.UUID) (room schema.Room) {
+	c.ServeHTTPOkAs("PUT", roomURL(roomID)+"/players", nil, &room)
+	return
+}
+
+func (c *RoomClient) getRoom(roomID uuid.UUID) (room schema.Room) {
+	c.ServeHTTPOkAs("GET", roomURL(roomID), nil, &room)
+	return
+}
+
+func roomURL(roomID uuid.UUID) string {
+	return "/rooms/" + roomID.String()
 }
 
 func TestRoomSuite(t *testing.T) {
