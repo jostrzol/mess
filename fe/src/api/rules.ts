9478d35c9b58b5dbe32ed03db855afc23deec22e
Jakub Ostrzołek <jakub.ostrzolek.stud@pw.edu.pl>
2023-11-24 12:56:08 +0100

    fe: fix format rules

--- fe/src/api/rules.ts
+++ fe/src/api/rules.ts
@@ -2,7 +2,7 @@ import { MessApi } from "./messApi";
 
 export class RulesApi extends MessApi {
   public format = async (src: string): Promise<string> => {
-    const res = await fetch("/rules/format", {
+    const res = await this.fetch("/rules/format", {
       method: "PUT",
       credentials: "include",
       body: src,
